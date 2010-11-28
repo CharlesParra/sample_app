6748662a604568aa926465df55d858a688382598
Charles Parra <charles@parra.com>
2010-11-28 10:40:39 -0500

    finished layout and routes?

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  
+  #return a title on a per-page basis
+  def title
+    base_title = "sample_app"
+
+    return base_title if @title.nil?
+    return "#{base_title} | #{@title}"
+  end
 end
