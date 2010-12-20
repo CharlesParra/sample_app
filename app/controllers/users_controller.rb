71f6108928313c90fb255e5658858dbdb2294f0e
Charles Parra <charles@parra.com>
2010-12-20 14:48:07 -0500

    up to page 376

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,11 @@
 class UsersController < ApplicationController
+  before_filter :authenticate, :only => [:index, :edit, :update]
+  before_filter :correct_user, :only => [:edit, :update]
+
+  def index
+    @title = "All users"
+    @users = User.paginate(:page => params[:page])
+  end
 
   def show
     @user = User.find(params[:id])
@@ -21,4 +28,27 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+  
+  def edit
+    @title = "Edit User"
+  end
+
+  def update
+    if @user.update_attributes(params[:user])
+      flash[:success] = "Profile updated."
+      redirect_to @user else
+      @title = "Edit user"
+      render 'edit'
+    end
+  end
+
+  private
+    def authenticate
+      deny_access unless signed_in?
+    end
+    
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
+    end
 end
